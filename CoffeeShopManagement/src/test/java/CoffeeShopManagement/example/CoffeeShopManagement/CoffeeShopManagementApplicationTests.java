db9bb01741aa062a092511a470ca856c3d1cb91a
ChisDong <dongminhchi9120@gmail.com>
2025-03-04 21:43:24 +0700

    commit 1

---
+++ CoffeeShopManagement/src/test/java/CoffeeShopManagement/example/CoffeeShopManagement/CoffeeShopManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package CoffeeShopManagement.example.CoffeeShopManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
